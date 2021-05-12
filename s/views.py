620535e2e3bafd67a3755f5d2e08e0046730033d
shilagit1234 <shilagit1234@gmail.com>
2021-05-12 08:13:11 +0530

    h

--- s/views.py
+++ s/views.py
@@ -9,3 +9,11 @@ from .models import Student
 class StudentApi(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import StudentSerializer
+from .models import Student
+class StudentApi(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
